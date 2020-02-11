478a7b37060603cc93332e7269e160b4fe23110e
nolan-industry <58996283+nolan-industry@users.noreply.github.com>
2020-02-11 11:52:13 +0000

    v3

--- pilotut.py
+++ pilotut.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
-
 from __future__ import print_function
-from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal
-import time
+
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
 from pymavlink import mavutil # Needed for command message definitions
+import time
 import math
 
 #change the first argument to your vehicle connection string
 vehicle = connect('tcp:127.0.0.1:5763', wait_ready=True)
 
-def arm_and_takeoff(TargetAltitude):
+
+def arm_and_takeoff(aTargetAltitude):
     """
-    Arms vehicle and fly to TargetAltitude.
+    Arms vehicle and fly to aTargetAltitude.
     """
 
     print("Basic pre-arm checks")
-    # Don't try to arm until autopilot is ready
+    # Don't let the user try to arm until autopilot is ready
     while not vehicle.is_armable:
         print(" Waiting for vehicle to initialise...")
         time.sleep(1)
@@ -25,27 +26,41 @@ def arm_and_takeoff(TargetAltitude):
     vehicle.mode = VehicleMode("GUIDED")
     vehicle.armed = True
 
-    # Confirm vehicle armed before attempting to take off
     while not vehicle.armed:
         print(" Waiting for arming...")
         time.sleep(1)
 
     print("Taking off!")
-    vehicle.simple_takeoff(TargetAltitude)  # Take off to target altitude
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
 
-    # Wait until the vehicle reaches a safe height before processing the goto
-    #  (otherwise the command after Vehicle.simple_takeoff will execute
-    #   immediately).
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
     while True:
         print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-        # Break and return from function just below target altitude.
-        if vehicle.location.global_relative_frame.alt >= TargetAltitude * 0.95:
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:  # Trigger just below target alt.
             print("Reached target altitude")
             break
         time.sleep(1)
 
 
-def absolute_heading(heading, relative=False):
+# Arm and take of to altitude of 5 meters
+arm_and_takeoff(5)
+
+"""
+Convenience functions for sending immediate/guided mode commands to control the Copter.
+
+The set of commands demonstrated here include:
+* MAV_CMD_CONDITION_YAW - set direction of the front of the Copter (latitude, longitude)
+* MAV_CMD_DO_SET_ROI - set direction where the camera gimbal is aimed (latitude, longitude, altitude)
+* MAV_CMD_DO_CHANGE_SPEED - set target speed in metres/second.
+
+
+The full set of available commands are listed here:
+http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/
+"""
+
+
+def condition_yaw(heading, relative=False):
     """
     Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
 
@@ -60,51 +75,22 @@ def absolute_heading(heading, relative=False):
     http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
     """
     if relative:
-        is_relative = 1   #yaw relative to direction of travel
+        is_relative = 1  # yaw relative to direction of travel
     else:
-        is_relative = 0  #yaw is an absolute angle
+        is_relative = 0  # yaw is an absolute angle
     # create the CONDITION_YAW command using command_long_encode()
     msg = vehicle.message_factory.command_long_encode(
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_CMD_CONDITION_YAW,   #command
-        0,  #confirmation
-        heading,    # param 1, yaw in degrees
-        0,          # param 2, yaw speed deg/s
-        1,          # param 3, direction -1 ccw, 1 cw
+        0, 0,  # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW,  # command
+        0,  # confirmation
+        heading,  # param 1, yaw in degrees
+        0,  # param 2, yaw speed deg/s
+        1,  # param 3, direction -1 ccw, 1 cw
         is_relative,  # param 4, relative offset 1, absolute angle 0
-        0, 0, 0)    # param 5 ~ 7 not used
+        0, 0, 0)  # param 5 ~ 7 not used
     # send command to vehicle
     vehicle.send_mavlink(msg)
 
-def relative_heading(heading, relative=True):
-    """
-    Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
-
-    This method sets an relative heading
-
-    By default the yaw of the vehicle will follow the direction of travel. After setting
-    the yaw using this function there is no way to return to the default yaw "follow direction
-    of travel" behaviour (https://github.com/diydrones/ardupilot/issues/2427)
-
-    For more information see:
-    http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
-    """
-    if relative:
-        is_relative = 1 #yaw relative to direction of travel
-    else:
-        is_relative = 0 #yaw is an absolute angle
-    # create the CONDITION_YAW command using command_long_encode()
-    msg = vehicle.message_factory.command_long_encode(
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
-        0, #confirmation
-        heading,    # param 1, yaw in degrees
-        0,          # param 2, yaw speed deg/s
-        1,          # param 3, direction -1 ccw, 1 cw
-        is_relative, # param 4, relative offset 1, absolute angle 0
-        0, 0, 0)    # param 5 ~ 7 not used
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
 
 def set_roi(location):
     """
@@ -117,14 +103,14 @@ def set_roi(location):
     """
     # create the MAV_CMD_DO_SET_ROI command
     msg = vehicle.message_factory.command_long_encode(
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_CMD_DO_SET_ROI, #command
-        0, #confirmation
-        0, 0, 0, 0, #params 1-4
+        0, 0,  # target system, target component
+        mavutil.mavlink.MAV_CMD_DO_SET_ROI,  # command
+        0,  # confirmation
+        0, 0, 0, 0,  # params 1-4
         location.lat,
         location.lon,
         location.alt
-        )
+    )
     # send command to vehicle
     vehicle.send_mavlink(msg)
 
@@ -317,7 +303,7 @@ The methods include:
 """
 
 
-def relative_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
+def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
     """
     Move vehicle in direction based on specified velocity vectors and
     for the specified duration.
@@ -350,7 +336,7 @@ def relative_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
         time.sleep(1)
 
 
-def absolute_global_velocity(velocity_x, velocity_y, velocity_z, duration):
+def send_global_velocity(velocity_x, velocity_y, velocity_z, duration):
     """
     Move vehicle in direction based on specified velocity vectors.
 
@@ -385,3 +371,4 @@ def absolute_global_velocity(velocity_x, velocity_y, velocity_z, duration):
     for x in range(0, duration):
         vehicle.send_mavlink(msg)
         time.sleep(1)
+
